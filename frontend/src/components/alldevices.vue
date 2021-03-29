bbfc5f63635dfcb8890ea1a0c12fb7cc04ad4320
phz <phz@saeros.in.phz.fi>
2021-03-29 16:30:25 +0300

    Added new node_modules, fixed Dockerfile

---
+++ frontend/src/components/alldevices.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+	<p>PEPEMAN</p>
+	</div>
+</template>
+
+<script>
+import { Doughnut } from 'vue-chartjs';
+export default {
+	extends: Doughnut,
+	name: "alldevices",
+  };
+</script>
+
+
+<style scoped>
+</style>
--- frontend/src/views/dashboard.vue
+++ frontend/src/views/dashboard.vue
@@ -3,13 +3,17 @@
 <template>
 	<div id="main">
 	<h1>THIS IS A TEMPORARY PAGE FOR THE FRONTPAGE DASHBOARD</h1>
+	<alldevices/>
 	</div>
 </template>
 
 <script>
+import alldevices from '../components/alldevices.vue'
 	export default {
 		name: 'dashboard',
-		};
+		components: { alldevices,
+		},
+	};
 </script>
 <style scoped>
 </style>
